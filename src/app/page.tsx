fa78bdd3ce6d767c4f9eb3d18271b205ccc686f1
Vikramjit Sarkar <101332966+VikramjitSarkar@users.noreply.github.com>
2026-01-16 02:40:58 +0530

    Update src/app/page.tsx via CodePasta IDE

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-4xl font-bold">Art Pasta</h1>
+      <p className="mt-4 text-gray-600">an online art gallary.  artist auth, user auth. any user can browse the app see all artist artwork. tap on artwork to see the image bigger and details of the art work, name, description, year, artist name, on sale ot not, price etc etc. home page multiple rails for types of art work. search page full text search of the art work and its details. profile page of user. to buy artwork and like artwork user need to sign up or login.  artist sign up or login - dashboard see all art work uploaded. on top button to ADD.  arttist can add multiple artworks, with all details. profiles setting page.  </p>
+    </main>
+  )
+}
